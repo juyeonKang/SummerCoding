e2fb929a3d29c83a81fe945b4bdf482c011d8c46
JY <lovedct_jy@naver.com>
2019-05-17 11:49:58 +0900

    Update tmp.py
    <수정1>--html_string 함수명 변경 반영
    <수정2>--testDB 데이터 읽어오기 (idx, title, content, done, duedate, ordernum)
    (( 이미지 표시 제외하고 todo 페이지 화면 뿌리기는 완성 ))
    (( done==0인 todo 항목 이동해서 ordernum 조정하는 로직 추가해야함 ))

--- tmp.py
+++ tmp.py
@@ -6,21 +6,22 @@ import sqlite3
 @route('/todo')
 def index():
     # html source code 틀
-    str_top = html.make_string()
-    str_bottom = html.make_string_end()
+    str_top = html.todo_top()
+    str_bottom = html.todo_bottom()
     str_todos = ""
 
-    # sqlite3 데이터 읽어와서 todos 요소 추가     
-    str_todos += html.new_todo(1,"할일 1111","내용111",0,"1996-11-04")
-    str_todos += html.new_todo(2,"title2","content2",0,"0000-00-00")
-    str_todos += html.new_todo(3,"title","content",1,"0000-01-15")
-    str_todos += html.new_todo(4,"t","c",0,"2019-05-20")
-    
+    # sqlite3 데이터 읽어와서 todos 요소 추가
+    conn = sqlite3.connect("testDB.db")
+    with conn:
+        c = conn.cursor()
+        c.execute("SELECT * FROM todo ORDER BY done ASC, ordernum ASC, duedate ASC")
+        rows = c.fetchall()
+        for data in rows:
+            str_todos += html.new_todo(data)
 
     # source = final html source code
     source = str_top + str_todos + str_bottom
 
     return source
-
 
 run(reloader=True, host='0.0.0.0', port = 8080)
